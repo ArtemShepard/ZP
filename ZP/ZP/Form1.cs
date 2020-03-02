71f674aa5314b6553bcd2ca96d85b41065c668c7
1 <1@303-13>
2020-03-02 17:51:21 +0500

    2

---
+++ ZP/ZP/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source = 303-13; Initial Catalog = Practica_3; Integrated Security=true;");//
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
